7eea8d973b8ce9e53e5fd2ccf4bfa4a3f823c79f
Borja_Lara_Sanchez <borjalarasanchez@gmail.com>
2026-03-02 00:14:36 +0100

    Add_Google_AdSense_script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 import "./globals.css";
@@ -27,6 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className="dark scroll-smooth">
+      <head>
+        <Script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5905230385471973" 
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0a0a0a] text-zinc-300 selection:bg-blue-500/20 selection:text-blue-400 flex flex-col min-h-screen font-sans`}
       >
